0712856ae2552db38f7b1a8fbb4126a410e9e1ca
Fisayo55 <adeyekunoluwafisayo01@gmail.com>
2024-10-06 09:08:29 +0100

    meals database added with better sqlite3

---
+++ app/meals/loading-out.js
@@ -0,0 +1,6 @@
+import classes from "../_styles/meals-loading-page.module.css";
+const LoadingOut = () => {
+  return <p className={classes.loading}>Fetching meals...</p>;
+};
+
+export default LoadingOut;
