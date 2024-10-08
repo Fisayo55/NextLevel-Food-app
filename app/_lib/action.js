6c3268c4f949464c9fc033cbf6748f567428678c
Fisayo55 <adeyekunoluwafisayo01@gmail.com>
2024-10-08 21:46:38 +0100

    save meals option added

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,6 +1,14 @@
 "use server";
 
-export const shareMeal = async (formData) => {
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
+
+const isInValidText = (text) => {
+  return !text.title || text.title.trim() === "";
+};
+
+export const shareMeal = async (prevState, formData) => {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -10,5 +18,19 @@ export const shareMeal = async (formData) => {
     creator_email: formData.get("email"),
   };
 
-  console.log(meal);
+  if (
+    isInValidText(meal.title) ||
+    isInValidText(meal.summary) ||
+    isInValidText(meal.instructions) ||
+    isInValidText(meal.creator) ||
+    isInValidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    message: "Invalid input";
+  }
+  await saveMeal(meal);
+  revalidatePath("/meals", "layout");
+  redirect("/meals");
 };
